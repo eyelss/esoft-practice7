1b90d053ae4f8d54ceebf9731b8fb2ea7b31a568
eyelss <stud0000200014@study.utmn.ru>
2025-05-25 08:08:59 +0500

    init

---
+++ grep-lite.js
@@ -0,0 +1,30 @@
+const fs = require('fs');
+const path = require('path');
+
+const args = Object.fromEntries(process.argv
+  .filter(arg => arg.startsWith('--'))
+  .map(arg => arg.slice(2).split('='))
+);
+
+const fileName = args['file'];
+const query = args['search'];
+
+if (!fileName || !query) {
+  throw new Error('Маловато аргументов.');
+}
+
+fs.readFile(path.join(__dirname, fileName), (err, data) => {
+  if (err) {
+    console.error(err);
+    return;
+  }
+
+  data
+    .toString()
+    .split('\n')
+    .forEach((line, idx) => {
+      if (line.includes(query)) {
+        console.log(`[Line ${idx+1}]: ${line}`);
+      }
+    });
+});
